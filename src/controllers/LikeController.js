75102e89e998e8c2dd7ccea43f86f2d42198a498
Thiago <thiagolourencosaraiva123@gmail.com>
2019-08-12 15:20:11 -0300

    Backend do tindev, desenvolvida em nodejs

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,34 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { devId } = req.params;
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    if (!targetDev) {
+      return res.status(401).json({ message: "Dev not exists" });
+    }
+
+    if (targetDev.likes.includes(loggedDev._id)) {
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit("match", targetDev);
+      }
+
+      if (targetDev) {
+        req.io.to(targetSocket).emit("match", loggedSocket);
+      }
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
